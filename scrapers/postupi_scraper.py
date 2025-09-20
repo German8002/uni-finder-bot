ea64b6ff59d4381b0b1ffba2df4afb235dfbd836
German8002 <germanlonkis@gmail.com>
2025-09-20 17:44:38 +0600

    Delete scrapers directory

--- scrapers/postupi_scraper.py
+++
@@ -1,5 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-def fetch_listings_for_search(q: str):
-    # placeholder: real scraping logic to search site would be implemented here
-    return []
